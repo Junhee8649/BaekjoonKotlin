b20b3702e2e51aedfd64561df4bfb617ba459c1b
Junhee-lee <sk5514224@gmail.com>
2025-01-16 16:39:13 +0900

    [feat] solve 2738

---
+++ src/2738.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val (n, m) = readln().split(" ").map { it.toInt() }
+    val matrixA = Array(n) { readln().split(" ").map { it.toInt() } }
+    val matrixB = Array(n) { readln().split(" ").map { it.toInt() } }
+
+    for (i in 0..<n) {
+        for (j in 0..<m) print("${matrixA[i][j] + matrixB[i][j]} ")
+        if(i != n-1) println()
+    }
+}
