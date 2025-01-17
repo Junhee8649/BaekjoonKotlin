0ba8388bf79662bb22fa436c6c6afc4ea2311b3c
Junhee-lee <sk5514224@gmail.com>
2025-01-17 16:23:39 +0900

    [feat] solve 9086

---
+++ src/9086.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val testNumber = readln().toInt()
+    for (i in 0..<testNumber){
+        val testString = readln()
+        if (i != testNumber-1) println("${testString.first()}${testString.last()}") else print("${testString.first()}${testString.last()}")
+    }
+}
