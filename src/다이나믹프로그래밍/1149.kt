c859d0d77410ae0fdf9fc3c8699ad840938b7282
Junhee-lee <sk5514224@gmail.com>
2025-01-19 22:58:57 +0900

    [feat] solve 1149

--- src/다이나믹프로그래밍/1149.kt
+++ src/다이나믹프로그래밍/1149.kt
@@ -1,5 +1,20 @@
 package 다이나믹프로그래밍
 
 fun main(){
+    val n = readln().toInt()
+    val dp = Array(n + 1){IntArray(3){0} }
 
+    val (r, g, b) = readln().split(" ").map { it.toInt() }
+    dp[1][0] = r
+    dp[1][1] = g
+    dp[1][2] = b
+
+    for (i in 2 .. n){
+        val (costR, costG, costB) = readln().split(" ").map { it.toInt() }
+        dp[i][0] = costR + minOf(dp[i - 1][1], dp[i - 1][2])
+        dp[i][1] = costG + minOf(dp[i - 1][0], dp[i - 1][2])
+        dp[i][2] = costB + minOf(dp[i - 1][0], dp[i - 1][1])
+    }
+
+    println(minOf(dp[n][0], dp[n][1], dp[n][2]))
 }
