a7bca76273c82e8e223e3de6542d17ace207a6c6
Junhee-lee <sk5514224@gmail.com>
2025-02-02 23:11:30 +0900

    [feat] solve 5339

---
+++ src/구현/5339.kt
@@ -0,0 +1,14 @@
+package 구현
+
+fun main() {
+    println("     /~\\ ")
+    println("    ( oo|")
+    println("    _\\=/_")
+    println("   /  _  \\")
+    println("  //|/.\\|\\\\")
+    println(" ||  \\ /  ||")
+    println("============")
+    println("|          |")
+    println("|          |")
+    println("|          |")
+}
