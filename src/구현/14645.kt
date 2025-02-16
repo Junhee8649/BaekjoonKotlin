1e97f858a1e6179a1eb3e50f066368cb72e455b5
Junhee-lee <sk5514224@gmail.com>
2025-02-16 23:51:58 +0900

    [feat] solve 14645

---
+++ src/구현/14645.kt
@@ -0,0 +1,9 @@
+package 구현
+
+fun main(){
+    val (n, k) = readln().split(" ").map { it.toInt() }
+    repeat(n){
+        val (a, b) = readln().split(" ").map { it.toInt() }
+    }
+    print("비와이")
+}
