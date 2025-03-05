38a5306f970ffa80c01e1a2f92a1010530ab6c52
Junhee-lee <sk5514224@gmail.com>
2025-03-05 13:24:58 +0900

    [feat] solve 2338

---
+++ src/수학/2338.kt
@@ -0,0 +1,14 @@
+package 수학
+
+import java.math.BigInteger
+
+fun main(){
+    val A = readln()
+    val B = readln()
+    val a = BigInteger(A)
+    val b = BigInteger(B)
+    println(a + b)
+    println(a - b)
+    println(a * b)
+}
+
