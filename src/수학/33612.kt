4b0990d5265fafc9e6d9e464ed4bda9b6dd84ed7
Junhee-lee <sk5514224@gmail.com>
2025-04-09 13:21:11 +0900

    solve 33612

---
+++ src/수학/33612.kt
@@ -0,0 +1,12 @@
+package 수학
+
+fun main(){
+    val n = readln().toInt()
+    when(n){
+        1 -> println("2024 8")
+        2 -> println("2025 3")
+        3 -> println("2025 10")
+        4 -> println("2026 5")
+        5 -> println("2026 12")
+    }
+}
